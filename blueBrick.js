7ff9998efd0fbc2663597fd6a557199a61dd5c0d
saptarshi2603 <73749321+saptarshi2603@users.noreply.github.com>
2021-02-04 13:29:44 +0530

    Add files via upload

---
+++ blueBrick.js
@@ -0,0 +1,28 @@
+class blueBrick{
+    constructor(x,y,width,height){
+    
+    var options={
+      isStatic:false,
+      restitution:0.4,
+      friction:0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+     
+    
+    }
+    
+    
+    display(){
+            push();
+            var pos = this.body.position
+           
+            rectMode(CENTER);
+            fill("blue")
+            rect(pos.x,pos.y,this.width,this.height)
+            pop();
+          }
+        }
---
+++ pinkBrick.js
@@ -0,0 +1,29 @@
+class pinkBrick{
+    constructor(x,y,width,height){
+    
+    var options={
+        isStatic:false,
+        restitution:0.4,
+        friction:0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+     
+    }
+    
+    display(){
+            push();
+            var pos = this.body.position
+           
+            rectMode(CENTER);
+            fill("pink")
+            rect(pos.x,pos.y,this.width,this.height)
+            pop();
+          }
+        
+    }
+    
+    
